57e3b66f9909fac4ba0f2897eb6f44e043a08fb2
Nabil Kadouri <kadourinabil7@gmail.com>
2025-09-03 10:25:35 +0200

    chore: Update project dependencies and configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,56 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'webandcrafts.com',
+          pathname: '/_next/image/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'cdn-icggj.nitrocdn.com',
+          pathname: '/AphBmykuaGyxZijWArNhxcCiPzVdYZGT/assets/images/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'www.extwebtech.com',
+          pathname: '/wp-content/uploads/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 's3.eu-west-2.amazonaws.com',
+          pathname: '/images.wiredmedia.co.uk/wp-content/uploads/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'assets.enterprisestorageforum.com',
+          pathname: '/uploads/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'www.mindinventory.com',
+          pathname: '/blog/wp-content/uploads/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'img.freepik.com',
+          pathname: '/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 't4.ftcdn.net',
+          pathname: '/jpg/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'admin.wac.co',
+          pathname: '/uploads/**',
+        },
+      ],
+    },
+  };
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 export default nextConfig;
